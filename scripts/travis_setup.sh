dded1f71a54fee75430cb19d20a90ec273a857f0
Roman Yurchak <rth@crans.org>
2016-04-19 01:00:33 +0200

    Playing with Travis

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -2,16 +2,16 @@
 
 if [[ $SIQT_BACKEND == 'PyQt4' ]]; then 
     if [[ ${TRAVIS_PYTHON_VERSION:0:1} == 2 ]]; then
-        conda install --yes "PyQT=4.11" $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+        conda install --yes "PyQT=4.11"
     elif [[ $TRAVIS_PYTHON_VERSION == "3.4" ]]; then
-        conda install -c https://conda.anaconda.org/dsdale24 pyqt5 $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+        conda install -c https://conda.anaconda.org/dsdale24 pyqt5 $DEPS
     else  # Python 3.5
-        conda install --yes -c https://conda.anaconda.org/mmcauliffe pyqt5 $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+        conda install --yes -c https://conda.anaconda.org/mmcauliffe pyqt5
     fi
 elif  [[ $SIQT_BACKEND == 'PyQt4' ]]; then
     if [[ ${TRAVIS_PYTHON_VERSION:0:1} == 2 ]]; then
-        conda install --yes "pyside" $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+        conda install --yes "pyside"
     else
-        conda install --yes -c pyzo pyzo-pyside $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+        conda install --yes -c pyzo pyzo-pyside
     fi
 fi
