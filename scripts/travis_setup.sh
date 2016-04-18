888ee66ed9396dd5b97947414b76a5682c4ac5ce
Roman Yurchak <rth@crans.org>
2016-04-19 01:29:52 +0200

    Added PyQt5 support on Py2.7

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -6,7 +6,7 @@ if [[ $SIQT_BACKEND == 'PyQt4' ]]; then
 elif [[ $SIQT_BACKEND == 'PyQt5' ]]; then 
     if [[ $TRAVIS_PYTHON_VERSION == "3.4" ]]; then
         conda install -c https://conda.anaconda.org/dsdale4 pyqt5
-    else  # Python 3.5
+    else  # Python 3.5 or 2.7
         conda install --yes -c https://conda.anaconda.org/spyder-ide pyqt5
     fi
 elif  [[ $SIQT_BACKEND == 'PySide' ]]; then
