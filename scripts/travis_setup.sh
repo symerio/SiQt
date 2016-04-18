6b3f3c872c00a34dd6ac0aa65f8edb3624a8e8ed
Roman Yurchak <rth@crans.org>
2016-04-19 01:25:37 +0200

    Fixing PyQt5

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -5,9 +5,9 @@ if [[ $SIQT_BACKEND == 'PyQt4' ]]; then
         conda install --yes "PyQT=4.11"
 elif [[ $SIQT_BACKEND == 'PyQt5' ]]; then 
     if [[ $TRAVIS_PYTHON_VERSION == "3.4" ]]; then
-        conda install -c https://conda.anaconda.org/dsdale24 pyqt5 $DEPS
+        conda install -c https://conda.anaconda.org/dsdale4 pyqt5
     else  # Python 3.5
-        conda install --yes -c https://conda.anaconda.org/mmcauliffe pyqt5
+        conda install --yes -c https://conda.anaconda.org/spyder-ide pyqt5
     fi
 elif  [[ $SIQT_BACKEND == 'PySide' ]]; then
     if [[ ${TRAVIS_PYTHON_VERSION:0:1} == 2 ]]; then
