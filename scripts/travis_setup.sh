174c7156d1e77c6451483a32c573440d36559fc2
Roman Yurchak <rth@crans.org>
2016-04-19 00:54:34 +0200

    Trying to fix Travis CI

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -2,14 +2,16 @@
 
 if [[ $SIQT_BACKEND == 'PyQt4' ]]; then 
     if [[ ${TRAVIS_PYTHON_VERSION:0:1} == 2 ]]; then
-        conda install --yes "PyQT=4.11"
-    else
-        conda install --yes -c https://conda.anaconda.org/dsdale24 pyqt5
+        conda install --yes "PyQT=4.11" $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+    elif [[ $TRAVIS_PYTHON_VERSION == "3.4" ]]; then
+        conda install -c https://conda.anaconda.org/dsdale24 pyqt5 $DEPS pip python=${TRAVIS_PYTHON_VERSION}
+    else  # Python 3.5
+        conda install --yes -c https://conda.anaconda.org/mmcauliffe pyqt5 $DEPS pip python=${TRAVIS_PYTHON_VERSION}
     fi
 elif  [[ $SIQT_BACKEND == 'PyQt4' ]]; then
     if [[ ${TRAVIS_PYTHON_VERSION:0:1} == 2 ]]; then
-        conda install --yes "pyside"
+        conda install --yes "pyside" $DEPS pip python=${TRAVIS_PYTHON_VERSION}
     else
-        conda install --yes -c pyzo pyzo-pyside
+        conda install --yes -c pyzo pyzo-pyside $DEPS pip python=${TRAVIS_PYTHON_VERSION}
     fi
 fi
