211421ac816a5b698c552262f7dc3beeb1ce4514
Roman Yurchak <rth@crans.org>
2016-04-19 19:37:41 +0200

    An unsatisfactory attempt to implement import hooks

--- SiQt/siqt/importers.py
+++ SiQt/siqt/importers.py
@@ -8,13 +8,10 @@ import os
 
 import pkgutil
 import os.path
+import six
+
 
 valid_backends = ['PyQt4', 'PyQt5', 'PySide']
-
-def get_path():
-
-    pkg = pkgutil.find_loader('PyQt5')
-    return os.path.dirname(pkg.path)
 
 def _normalise_name(name):
     for backend in valid_backends:
@@ -42,14 +39,110 @@ def use(backend_name, force=False, mode='smooth', matplotlib_hook=False):
      - matplotlib_hook: call matplotlib.use with the correct backend
             when matplotlib is first imported
     """
+    import sys
     from .. import this
     name = _normalise_name(backend_name)
     if this.backend is not None:
-        print('Warning: SiQt.use(backend_name) should be called before any imports of PyQt.\n'\
-                'Calling this a second time will have no effect!')
+        if this.backend != name:
+            print('Warning: SiQt.use should be called before any imports of PyQt/PySide.\n'\
+              '         Changing backend on the runtime (i.e. calling it a second time) is not supported!')
         return
     this.backend = name
     pkg = pkgutil.find_loader(name)
-    path_new = os.path.dirname(pkg.path)
+    if six.PY3:
+        path_new = os.path.dirname(pkg.path)
+    else:
+        path_new = os.path.dirname(pkg.filename)
     this.__path__.append(path_new)
+    sys.meta_path.insert(0, HijackPyQtImport())
+    if matplotlib_hook:
+        sys.meta_path.insert(0, MatplotlibImporter())
 
+
+class MatplotlibImporter(object):
+    def __init__(self):
+        pass
+
+    def find_module(self, fullname, path=None):
+        import sys
+        if fullname.startswith('matplotlib'):
+            sys.meta_path = sys.meta_path[1:] # remove the matplotlib hook
+            # now initialize matplotlib with the correct Qt4 backend
+            from . import matplotlib
+        return None
+
+    def load_module(self, name):
+        """This is never going to be called"""
+        pass
+
+def _uncheck_name(func):
+    def wrapper(name):
+        print(name)
+        return func('SiQt')
+    return wrapper
+
+
+import sys
+
+def name(item):
+    " Return an item's name. "
+    return item.__name__
+    
+def format_arg_value(arg_val):
+    """ Return a string representing a (name, value) pair.
+    
+    >>> format_arg_value(('x', (1, 2, 3)))
+    'x=(1, 2, 3)'
+    """
+    arg, val = arg_val
+    return "%s=%r" % (arg, val)
+
+
+def echo(fn, write=sys.stdout.write):
+    """ Echo calls to a function.
+    
+    Returns a decorated version of the input function which "echoes" calls
+    made to it by writing out the function's name and the arguments it was
+    called with.
+    """
+    import functools
+    # Unpack function's arg count, arg names, arg defaults
+    @functools.wraps(fn)
+    def wrapped(*v):
+        # Collect function arguments by chaining together positional,
+        # defaulted, extra positional and keyword arguments.
+        v = list(v)
+        if v[0] in valid_backends:
+            v[0] = 'SiQt'
+        write("%s,%s\n" % (v, type(v)))
+        return fn(*v)
+    return wrapped
+
+
+
+class HijackPyQtImport(object):
+
+    def find_module(self, fullname, path=None):
+        import sys
+        from .. import this
+        for backend_name in valid_backends:
+            if fullname.startswith(backend_name):
+                fullname_out = fullname.replace(backend_name, 'SiQt')
+                print(backend_name, fullname,  path, fullname_out)
+                print(sys.meta_path)
+                for importer in sys.meta_path:
+                    if isinstance(importer, HijackPyQtImport):
+                        continue
+                    loader = importer.find_module(fullname_out, path)
+                    print('1. ', importer, loader, loader is None)
+                    if loader is not None:
+                        for method in dir(loader):
+                            if not method.startswith('__'):
+                                setattr(loader, method, echo(getattr(loader, method)))
+                            #print(el, getattr(loader, el))
+                        #loader.exec_module = _uncheck_name(loader.exec_module)
+                        return loader
+                else:
+                    raise ValueError('This should not be possible!')
+        else:
+            return None
