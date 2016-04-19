3b1478c922b8ee7bc1d0e3862c88ac219230e117
Roman Yurchak <rth@crans.org>
2016-04-19 14:14:52 +0200

    Changed package hierarchy

---
+++ SiQt/siqt/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from . import dep_resolv
+from . import definitions
+from . import logic_parser
+from .qtbase import *
+from .deployement import  _resource_path
+
+from ._version import __version__
+
+
+__version_date__ = "Sun Feb 14 14:28:51 2016 +0100"
+__version_hash__ = "1d5f7f3"
