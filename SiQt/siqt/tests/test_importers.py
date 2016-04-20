9f72a17206360277ce3f951370db036480b43219
Roman Yurchak <rth@crans.org>
2016-04-20 14:25:15 +0200

    Working to run tests in a separate process

--- SiQt/siqt/tests/test_importers.py
+++ SiQt/siqt/tests/test_importers.py
@@ -5,8 +5,35 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import functools
 from unittest.case import SkipTest
+from multiprocessing import Process, Queue
+from time import sleep, time
 
+
+def _run_process(func):
+    @functools.wraps(func)
+    def inner_wrapper(*args):
+        def func_wrapper(func, q, args):
+            try:
+                res = func(*args)
+                q.put(res)
+            except Exception as e:
+                q.put(e)
+
+        q = Queue()
+        p = Process(target=func_wrapper, args=(func, q, args))
+        p.start()
+
+        result = q.get(block=True)
+        if isinstance(result, Exception):
+            raise result
+
+        return result
+    return inner_wrapper
+
+
+@_run_process
 def _check_importer(backend):
     import SiQt
     try:
@@ -32,4 +59,3 @@ def test_PyQt5_importer():
 
 def test_PySide_importer():
     _check_importer('PySide')
-
