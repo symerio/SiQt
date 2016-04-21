4269ec94e5135896b7b2ee87de3f2255b77058ee
Roman Yurchak <rth@crans.org>
2016-04-21 16:27:56 +0200

    Cleaned up the importers stuff

--- SiQt/siqt/importers.py
+++ SiQt/siqt/importers.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
 import os
 
 import pkgutil
@@ -102,14 +103,8 @@ class MatplotlibImporter(object):
 
 class RenameImportFinder(object):
     """
-    A class for import hooks mapping Py3 module names etc. to the Py2 equivalents.
     Adapted from http://python-future.org/_modules/future/standard_library.html
     """
-    # Different RenameImport classes are created when importing this module from
-    # different source files. This causes isinstance(hook, RenameImport) checks
-    # to produce inconsistent results. We add this RENAMER attribute here so
-    # remove_hooks() and install_hooks() can find instances of these classes
-    # easily:
     def __init__(self):
         pass
 
@@ -123,15 +118,8 @@ class RenameImportFinder(object):
 
 class RenameImportLoader(object):
     """
-    A class for import hooks mapping Py3 module names etc. to the Py2 equivalents.
     Adapted from http://python-future.org/_modules/future/standard_library.html
     """
-    # Different RenameImport classes are created when importing this module from
-    # different source files. This causes isinstance(hook, RenameImport) checks
-    # to produce inconsistent results. We add this RENAMER attribute here so
-    # remove_hooks() and install_hooks() can find instances of these classes
-    # easily:
-    RENAMER = True
 
     def __init__(self, name, path=None):
         '''
@@ -144,21 +132,21 @@ class RenameImportLoader(object):
     def load_module(self, name, path=None):
         if path is None:
             path = self.path
-        print('Loading', name, path)
+        #print('Loading', name, path)
         for backend_name in valid_backends:
             if backend_name in name:
                 name = name.replace(backend_name, 'SiQt')
                 self.name_new = name
                 break
-        print('2. Loading', name)
+        #print('2. Loading', name)
         if name in sys.modules:
-            print('zzzz', name)
-            print(sys.modules.keys())
+            #print('zzzz', name)
+            #print(sys.modules.keys())
             module = sys.modules[name]
             if self.name_orig not in sys.modules:
                 sys.modules[self.name_orig] = module
         else:
-            print('aaa')
+            #print('aaa')
             module = self._find_and_load_module(name, path)
             sys.modules[name] = module
             sys.modules[self.name_orig] = module
@@ -178,7 +166,7 @@ class RenameImportLoader(object):
         while len(bits) > 1:
             # Treat the first bit as a package
             packagename = bits.pop(0)
-            print(packagename)
+            #print(packagename)
             package = self._find_and_load_module(packagename, path)
             try:
                 path = package.__path__
@@ -192,17 +180,16 @@ class RenameImportLoader(object):
         #if six.PY2:
         name = bits[0]
         if name in sys.modules:
-            print('zzzz', name, six.PY2)
-            #print(sys.modules.keys())
+            #print('zzzz', name, six.PY2)
             #sys.modules[self.name_orig] = name
             module = sys.modules[name]
             #print(module.__spec__)
         else:
-            print('4.', name, path)
+            #print('4.', name, path)
             fp, pathname, description = imp.find_module(name, path)
             print(name, self.name_new,  fp, pathname, description)
             try:
-                print(self.name_orig)
+                #print(self.name_orig)
                 module = imp.load_module(self.name_orig, fp, pathname, description)
             except Exception as e:
                 #if six.PY2:
@@ -222,76 +209,78 @@ class RenameImportLoader(object):
 
 
 
-import sys
 
-def name(item):
-    " Return an item's name. "
-    return item.__name__
-    
-def format_arg_value(arg_val):
-    """ Return a string representing a (name, value) pair.
-    
-    >>> format_arg_value(('x', (1, 2, 3)))
-    'x=(1, 2, 3)'
-    """
-    arg, val = arg_val
-    return "%s=%r" % (arg, val)
-
-
-def echo(fn):
-    """ Echo calls to a function.
-    
-    Returns a decorated version of the input function which "echoes" calls
-    made to it by writing out the function's name and the arguments it was
-    called with.
-    """
-    import functools
-    # Unpack function's arg count, arg names, arg defaults
-    @functools.wraps(fn)
-    def wrapped(*v):
-        # Collect function arguments by chaining together positional,
-        # defaulted, extra positional and keyword arguments.
-        v = list(v)
-        name = v[0]
-        if name in valid_backends:
-            v[0] = 'SiQt'
-        elif name == 'SiQt':
-            pass
-        elif hasattr(name, '__spec__'):
-            print(dir(name))
-            spec = name.__spec__
-            spec.name = 'SiQt'
-            name.__name__ = 'SiQt'
-        else:
-            print(name)
-
-        print("%s %s" % (fn, v))
-        res = fn(*v)
-        print("Out: %s" % res)
-        return res
-    return wrapped
-
-
-class HijackPyQtImport(object):
-
-    def find_module(self, fullname, path=None):
-        import sys
-        from .. import this
-        for backend_name in valid_backends:
-            if fullname.startswith(backend_name):
-                fullname_out = fullname.replace(backend_name, 'SiQt')
-                print(backend_name, fullname,  path, fullname_out)
-                print(sys.meta_path)
-                if six.PY3:
-                    import importlib.machinery
-                    print(fullname_out, path)
-                    loader =  importlib.machinery.SourceFileLoader(fullname_out, path)
-                    for attr in dir(loader):
-                        if not attr.startswith('__'):
-                            setattr(loader, attr, echo(getattr(loader, attr)))
-                    return loader
-                    #return self._py3_loader(fullname_out, path)
-                else:
-                    raise NotImplementedError
-        else:
-            return None
+## some earlier attemps should be removed
+#import sys
+#
+#def name(item):
+#    " Return an item's name. "
+#    return item.__name__
+#    
+#def format_arg_value(arg_val):
+#    """ Return a string representing a (name, value) pair.
+#    
+#    >>> format_arg_value(('x', (1, 2, 3)))
+#    'x=(1, 2, 3)'
+#    """
+#    arg, val = arg_val
+#    return "%s=%r" % (arg, val)
+#
+#
+#def echo(fn):
+#    """ Echo calls to a function.
+#    
+#    Returns a decorated version of the input function which "echoes" calls
+#    made to it by writing out the function's name and the arguments it was
+#    called with.
+#    """
+#    import functools
+#    # Unpack function's arg count, arg names, arg defaults
+#    @functools.wraps(fn)
+#    def wrapped(*v):
+#        # Collect function arguments by chaining together positional,
+#        # defaulted, extra positional and keyword arguments.
+#        v = list(v)
+#        name = v[0]
+#        if name in valid_backends:
+#            v[0] = 'SiQt'
+#        elif name == 'SiQt':
+#            pass
+#        elif hasattr(name, '__spec__'):
+#            print(dir(name))
+#            spec = name.__spec__
+#            spec.name = 'SiQt'
+#            name.__name__ = 'SiQt'
+#        else:
+#            print(name)
+#
+#        print("%s %s" % (fn, v))
+#        res = fn(*v)
+#        print("Out: %s" % res)
+#        return res
+#    return wrapped
+#
+#
+#class HijackPyQtImport(object):
+#
+#    def find_module(self, fullname, path=None):
+#        import sys
+#        from .. import this
+#        for backend_name in valid_backends:
+#            if fullname.startswith(backend_name):
+#                fullname_out = fullname.replace(backend_name, 'SiQt')
+#                print(backend_name, fullname,  path, fullname_out)
+#                print(sys.meta_path)
+#                if six.PY3:
+#                    import importlib.machinery
+#                    print(fullname_out, path)
+#                    loader =  importlib.machinery.SourceFileLoader(fullname_out, path)
+#                    for attr in dir(loader):
+#                        if not attr.startswith('__'):
+#                            setattr(loader, attr, echo(getattr(loader, attr)))
+#                    return loader
+#                    #return self._py3_loader(fullname_out, path)
+#                else:
+#                    raise NotImplementedError
+#        else:
+#            return None
